1df1cb96a2c8406d12626c7b736cae20f1a59958
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-17 08:31:06 -0700

    First Commit

---
+++ resources/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+    
+    /* For the sticky navigation */
+    
+    
+    
+    /* Scroll on buttons */
+    
+    
+    
+    /* Navigation scroll */
+    
+    
+    
+    /* Animations on scroll */
+    
+    
+    
+    /* Mobile navigation */
+    
+});
