a08a03f59da3b40ce7dd96923ee27af4bbfe79d6
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-22 17:21:21 -0700

    Sticky navbar added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     
-    
-    /* For the sticky navigation */
-    
+    /* For the sticky navigation  - Using waypoint plugin */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
     
     
     /* Scroll on buttons */
