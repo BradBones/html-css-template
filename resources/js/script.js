c4f56b8f69c00fe96cef9e443f7b0b9511950317
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-23 08:45:47 -0700

    Mobile navbar added

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,19 +12,86 @@ $(document).ready(function() {
     });
 
     
-    
     /* Scroll on buttons */
-    
+
+    /////////////////
+    // HEADER BUTTONS
+    /////////////////
+
+    // "I'm hungry" button points to sign-up plans section.
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    // "Show Me More" button points to features section.
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
     
     
     /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+                }
+            }
+        });
+    });
     
     
+    /* Animations on scroll - Using the waypoint plugin */
+
+    // Features section fade-in
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%;'
+    });
+    // 
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%;'
+    });
+    // Cities fade-in
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%;'
+    });
+    // Sign in plans pulse to draw attention
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%;'
+    });
     
-    /* Animations on scroll */
     
-    
-    
-    /* Mobile navigation */
+    /* Mobile navigation  - Open/close effect & switch icon */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
+
+
+
     
 });
